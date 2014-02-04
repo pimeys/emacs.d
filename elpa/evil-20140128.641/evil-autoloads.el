8754b2acc8fbb8a2baf695e4900412cf5c26114c
Julius de Bruijn <julius.bruijn@sponsorpay.com>
2014-02-04 18:32:35 +0100

    Fix merge hell

--- elpa/evil-20140128.641/evil-autoloads.el
+++
@@ -1,60 +0,0 @@
-<<<<<<< Updated upstream
-;;; evil-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "evil-core" "evil-core.el" (21226 40623 353328
-;;;;;;  128000))
-;;; Generated autoloads from evil-core.el
- (autoload 'evil-mode "evil" "Toggle evil in all buffers" t)
-
-;;;***
-
-;;;### (autoloads nil nil ("evil-commands.el" "evil-common.el" "evil-digraphs.el"
-;;;;;;  "evil-ex.el" "evil-integration.el" "evil-macros.el" "evil-maps.el"
-;;;;;;  "evil-pkg.el" "evil-repeat.el" "evil-search.el" "evil-states.el"
-;;;;;;  "evil-types.el" "evil-vars.el" "evil.el") (21226 40623 379483
-;;;;;;  542000))
-
-;;;***
-
-(provide 'evil-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; evil-autoloads.el ends here
-||||||| merged common ancestors
-=======
-;;; evil-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "evil-core" "evil-core.el" (21226 29419 833149
-;;;;;;  932000))
-;;; Generated autoloads from evil-core.el
- (autoload 'evil-mode "evil" "Toggle evil in all buffers" t)
-
-;;;***
-
-;;;### (autoloads nil nil ("evil-commands.el" "evil-common.el" "evil-digraphs.el"
-;;;;;;  "evil-ex.el" "evil-integration.el" "evil-macros.el" "evil-maps.el"
-;;;;;;  "evil-pkg.el" "evil-repeat.el" "evil-search.el" "evil-states.el"
-;;;;;;  "evil-types.el" "evil-vars.el" "evil.el") (21226 29419 870292
-;;;;;;  270000))
-
-;;;***
-
-(provide 'evil-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; evil-autoloads.el ends here
->>>>>>> Stashed changes
